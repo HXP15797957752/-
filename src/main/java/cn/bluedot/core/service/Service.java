775841521079e3e32784e62dcc6c8f51e4c36d06
zhangguozhangyue <1181601469@qq.com>
2018-09-09 20:27:45 +0800

    母猪和仓库service

--- src/main/java/cn/bluedot/core/service/Service.java
+++ src/main/java/cn/bluedot/core/service/Service.java
@@ -1,10 +1,12 @@
 package cn.bluedot.core.service;
 
+import cn.bluedot.framemarker.dao.SuperDao;
+
 /**
  * 所有的service类继承此类
  * */
 public interface Service {
-    
+    SuperDao superDao=new SuperDao();
 }
 
 

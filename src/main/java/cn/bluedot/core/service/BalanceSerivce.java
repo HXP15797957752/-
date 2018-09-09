df89454d879999f77b6af57319f3b4b06f887737
youbinping <1259275929@qq.com>
2018-09-09 20:33:17 +0800

    养户结算

---
+++ src/main/java/cn/bluedot/core/service/BalanceSerivce.java
@@ -0,0 +1,74 @@
+package com.service;
+
+import java.util.Map;
+
+/**
+ * @author : 游斌平
+ *
+ */
+public class BalanceSerivce {
+	/**
+	 * 上传信息
+	 * @param data:传输过来的边界类的数据
+	 * @return  :执行情况，代表转发页面、数据
+	 */
+	public String insertInformation(Map<String,String[]> data) {
+		return null;
+	}
+	/**
+	 * 删除信息
+	 * @param data :传输过来的边界类的数据
+	 * @return  执行情况，代表转发页面、数据
+	 */
+	public String deleteInformmation(Map<String, String[]> data) {
+		return null;
+	}
+	/**
+	 * 更新信息
+	 * @param data:传输过来的边界类的数据
+	 * @return  :执行情况，代表转发页面、数据
+	 */
+	public String updateInformation(Map<String, String[]> data) {
+		return null;
+	}
+	/**
+	 * 查找信息
+     * @param data:传输过来的边界类的数据
+	 * @return  :执行情况，代表转发页面、数据
+	 */
+	public String searchInformation(Map<String, String[]> data) {
+		return null;
+	}
+	/**
+	 * 数据显示
+	 * @param data:传输过来的边界类的数据
+	 * @return  :执行情况，代表转发页面、数据
+	 */
+	public String showData(Map<String, String[]> data) {
+		return null;
+	}
+	/**
+	 * 导入
+	 * @param data:传输过来的边界类的数据
+	 * @return  :执行情况，代表转发页面、数据
+	 */
+	public String toLead(Map<String, String[]> data) {
+		return null;
+	}
+	/**
+	 * 导出
+	 * @param data:传输过来的边界类的数据
+	 * @return  :执行情况，代表转发页面、数据
+	 */
+	public String export(Map<String, String[]> data) {
+		return null;
+	}
+	/**
+	 * 转账
+	 * @param data:传输过来的边界类的数据
+	 * @return  :执行情况，代表转发页面、数据
+	 */
+	public String transferAccounts(Map<String, String[]> data) {
+		return null;
+	}
+}
